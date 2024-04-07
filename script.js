2117a434b2e5c6cc4e0f7dadd9b8274cb01e55ae
Naman0133 <64341872+Naman0133@users.noreply.github.com>
2024-04-08 00:13:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+  const items = document.querySelectorAll('.item');
+  e.target.matches('.next') && slider.append(items[0]);
+}
+document.addEventListener('click',activate,false);
